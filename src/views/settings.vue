7b403dad6311598e9d26e786c5ef4dd4411d6260
yunyuyuan <me@yunyuyuan.net>
2024-04-25 09:24:54 +0800

    chore: git tag as  version

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { inject, onMounted, toRaw } from 'vue';
-// @ts-ignore
-import { GM_info } from 'monkeyClient';
 import { SettingsKeyAutoDeleteDay, SettingsKeyAutoDelete404, SettingsKeyFoldItem, SettingsStorageKey, DefaultSettings } from '../constants';
 import { Settings } from '../types';
 
+const version = import.meta.env.jandan_recorder_version_name;
 const settings = inject<Settings>("settings")!;
 
 const refreshSettings = () => {
@@ -69,8 +68,8 @@ onMounted(() => {
             <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
           </svg>
         </a>
-      </p><br/>
-      <p>当前版本：{{ GM_info.script.version }}<span style="color: grey;margin: 0 10px">|</span><a target="_blank" href="https://update.greasyfork.org/scripts/488975/%E7%85%8E%E8%9B%8B%E5%90%90%E6%A7%BD%E8%AE%B0%E5%BD%95%E5%99%A8.user.js">检查更新</a></p>
+      </p>
+      <p>当前版本：{{ version }}<span style="color: grey;margin: 0 10px">|</span><a target="_blank" href="https://update.greasyfork.org/scripts/488975/%E7%85%8E%E8%9B%8B%E5%90%90%E6%A7%BD%E8%AE%B0%E5%BD%95%E5%99%A8.user.js">检查更新</a></p>
     </div>
   </div>
 </template>
@@ -79,6 +78,7 @@ onMounted(() => {
 .settings-container {
   width: 100%;
   overflow: auto;
+  text-align: center;
   > div {
     padding: 20px 0;
     border-bottom: 1px solid gray;
